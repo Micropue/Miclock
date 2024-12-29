d71a87bc57b568dbc10e6f13b812c85a901b3689
Micropue <121767335+Micropue@users.noreply.github.com>
2024-12-29 13:30:29 +0800

    更新 index.js
    添加注释

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+//引入exec库
 const { exec } = require('child_process')
+//引入systeminformation库
 const si = require('systeminformation')
 //获取系统电量
 function getbattery() {
@@ -20,7 +22,9 @@ class __battery extends HTMLElement {
         this.querySelector("p").innerHTML = `${value}`
     }
 }
+//自定义元素
 customElements.define("m-battery", __battery)
+//对象为使用到的元素
 const ele = {
     time: {
         big: document.querySelector("#time #big"),
@@ -32,6 +36,7 @@ const ele = {
     memory: document.querySelector(".memory"),
     system: document.querySelector("#system")
 }
+//更新电量的封装1秒一次
 const battery = {
     init() {
         setInterval(() => {
@@ -44,6 +49,7 @@ const battery = {
 }
 
 
+//更新时间的封装包含init方法和格式化format方法
 const time = {
     init() {
         setInterval(() => {
@@ -58,15 +64,20 @@ const time = {
         }, 1000)
     },
     format(time) {
+        //小于10的数就在数字前面补一个0
         return time < 10 ? "0" + time : time
     }
 }
+//更新日期包含init，get，formatweek方法
 const date = {
     init() {
+        //从get方法中获取月日周
         const { month, day, week } = this.get()
+        //初始化，以免在加载完成时仍要等待一秒
         ele.date.innerHTML = `${month}-${day} ${this.formatweek(week)}`
         ele.date.classList.add("show")
         setInterval(() => {
+            //1秒刷新一次
             const { month, day, week } = this.get()
             ele.date.innerHTML = `${month}-${day} ${this.formatweek(week)}`
         }, 1000)
@@ -76,35 +87,47 @@ const date = {
         const month = now.getMonth() + 1
         const day = now.getDate()
         const week = now.getDay()
+        //返回月日周
         return { month, day, week }
     },
+    //格式化星期，根据day参数的数字定位到数组的元素。
     formatweek(day) {
         const week = [ "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
         return week[day]
     }
 }
-
+//自定义元素用来显示存储，继承HTMLElement原型
 class __mRang extends HTMLElement {
+    //类构造器
     constructor() {
+        //继承属性
         super()
+        //定义类的全局值，初始化是必须的规范。
         this.value = undefined
     }
     connectedCallback() {
+        //自定义元素被添加到页面后的操作
         const ele = document.createElement("m-value")
         this.value = this.innerHTML
+        //通过css变量为条条赋值
         ele.style.setProperty("--value", `${this.innerHTML}`)
         this.innerHTML = ""
         this.append(ele)
     }
     show() {
+        //显示该元素
         this.querySelector("m-value").classList.add("show")
     }
 }
+//自定义元素
 customElements.define("m-rang", __mRang)
+//获取系统信息
+//系统信息都是通过systeminformation获取的，但是返回的都是promise。都是异步的。
 function getSystemInfor() {
-    var cpu, fssize = { total: 0, used: 0 }
+    var cpu, fssize = { total: 0, used: 0 }//这段不要也罢在最后返回的时候直接返回也可。
     return new Promise((res) => {
-        si.cpu().then(res => cpu = `${res.vendor} ${res.brand}`).then(() => {
+        //返回promise。
+        si.cpu().then(res => cpu = `${res.vendor} ${res.brand}`).then(() => {//等待执行完成后执行作用域内的操作
             si.mem().then(res => cpu += ` ${res.total / 1024 / 1024 / 1024}GB`).then(() => {
                 si.fsSize().then(res => {
                     fssize.total = res[0].size / 1024 / 1024 / 1024
@@ -112,7 +135,7 @@ function getSystemInfor() {
                 }).then(()=>{
                     res({ cpu, fssize })
                 })
-            })
+            })//多层嵌套不是很好。
         })
     })
 }
@@ -122,6 +145,7 @@ setTimeout(() => {
     battery.init()
     time.init()
     getSystemInfor().then(res => {
+        //获取系统信息promise的返回
         const s = res
         ele.system.querySelector(".cpu").innerHTML = res.cpu
         ele.memory.querySelector("p").innerHTML = `${parseInt(s.fssize.used)}GB / ${parseInt(s.fssize.total)}GB`
@@ -133,4 +157,4 @@ setTimeout(() => {
             rang.querySelector("m-value").classList.add("show")
         },500)
     })
-}, 5000)
+}, 5000)//在开屏动画执行结束后执行
